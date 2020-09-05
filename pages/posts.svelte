9bbbe2813cfe50c349b4ba7840829477ada4e413
josefaidt <josef.aidt@gmail.com>
2020-09-05 10:16:05 -0500

    add sorting, input options to allPosts resolver

---
+++ pages/posts.svelte
@@ -0,0 +1,56 @@
+<script>
+  import { url } from '@roxi/routify'
+  const query = `
+    query ALL_POSTS {
+      allPosts(data:{}) {
+        _id
+        slug
+        frontmatter {
+          title
+          date
+          published
+          tags
+        }
+      }
+    }
+  `
+</script>
+
+<section>
+  <h1>Snakes and Sparklers</h1>
+  <p>welcome to the blog</p>
+  <h2>Posts</h2>
+  <!-- prettier-ignore -->
+  {#each query.allPosts as post}
+  <a href="{$url(post.slug)}" aria-labelledby={post._id}>
+    <article>
+        <!-- need unique ID's, generate with uuid -->
+        <h2 id={post._id}>{post.frontmatter.title}</h2>
+        <p>{new Date(post.frontmatter.date).toLocaleDateString()}</p>
+      
+    </article>
+    </a>
+  {/each}
+</section>
+
+<style>
+  a,
+  article {
+    color: var(--theme-text);
+    text-decoration: none;
+  }
+
+  article {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    margin: 1rem 0;
+    transition: transform 0.3s ease;
+    padding: 1rem;
+  }
+
+  article:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    transform: translateY(-5px);
+  }
+</style>
--- support/graphql/resolvers.js
+++ support/graphql/resolvers.js
@@ -1,35 +1,60 @@
 const { promises: fs } = require('fs')
 const path = require('path')
 const fm = require('front-matter')
+const { v4: uuid } = require('uuid')
 const recursiveReadDir = require('../recursiveReadDir')
 
 module.exports = {
   allPosts: async (args, ctx, info) => {
     let result = []
     let postPath
-    if (args.directory) {
-      postPath = path.resolve(args.directory)
+    if (args.data.directory) {
+      postPath = path.resolve(args.data.directory)
     } else {
       const base = process.cwd()
-      postPath = path.join(base, 'pages/blog')
+      postPath = path.join(base, 'pages/posts')
     }
     const posts = await recursiveReadDir(postPath)
     for (let post of posts) {
       // This will give you a valid svelte component
       let { attributes: frontmatter } = fm(await fs.readFile(post, 'utf8'))
-      let slug = post.replace(postPath, '/blog')
+      let slug = post.replace(postPath, '/posts')
       if (slug.endsWith('/index.svx')) slug = slug.replace(/\/index\.svx$/g, '')
       else if (slug.endsWith('.svx')) slug = slug.replace('.svx', '')
 
-      if (frontmatter.date) frontmatter.date = new Date(`${frontmatter.date}`).toDateString()
+      if (frontmatter.date) frontmatter.date = new Date(`${frontmatter.date}`).toString()
       // push prepped node data
       result.push({
+        _id: uuid(),
         name: path.basename(post).replace(path.extname(post), ''),
         ext: path.extname(post).replace(/^\./g, ''),
         absolutePath: post,
         slug,
         frontmatter,
       })
+    }
+
+    // prep result based on supplied sort
+    if (args.data.sortBy && args.data.order) {
+      let { sortBy, order } = args.data
+      if (!['asc', 'desc'].includes(order.toLowerCase())) {
+        throw new Error('Invalid sort order supplied, must be one of "ASC" or "DESC"')
+      }
+
+      const pre = prop => {
+        if (sortBy.toLowerCase() === 'date') {
+          return new Date(prop)
+        }
+        return prop
+      }
+
+      if (order.toLowerCase() === 'asc') {
+        result.sort((a, b) => (pre(a.frontmatter[sortBy]) < pre(b.frontmatter[sortBy]) ? -1 : 1))
+      }
+
+      if (order.toLowerCase() === 'desc') {
+        result.sort((a, b) => (pre(a.frontmatter[sortBy]) > pre(b.frontmatter[sortBy]) ? -1 : 1))
+      }
     }
     // console.log('RESULT', result)
     return result
