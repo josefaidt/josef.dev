2aedd25c21e641c401bb0e1eb116ee2908dbbb68
josefaidt <josef.aidt@gmail.com>
2022-01-17 09:20:02 -0600

    remove unused packages

--- app.config.js
+++ app.config.js
@@ -1,31 +1,8 @@
-import { resolve } from 'path'
-
-// not in use
-const markdown = {
-  rehypePlugins: [
-    [
-      'cloudinary', // replace with actual package, consider app-specific plugins
-      {
-        baseDir: resolve('content/posts'),
-        uploadFolder: 'josef.dev',
-        transformations: 'q_auto,f_auto',
-      },
-    ],
-  ],
-}
-
-const app = {
+export default {
   title: 'josef',
   url: 'https://josef.dev',
   description: 'Welcome to my personal site',
   keywords: ['josef', 'aidt', 'personal', 'portfolio', 'svelte'],
   author: 'Josef Aidt',
   handle: 'josefaidt',
-  cloudinaryConfig: {
-    baseDir: resolve('content/posts'),
-    uploadFolder: 'josef.dev',
-    transformations: 'q_auto,f_auto',
-  },
 }
-
-export default app
