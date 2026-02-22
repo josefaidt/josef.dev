1ccf59015d3028ea5e50f297fe7d182838b1c633
josefaidt <josef.aidt@gmail.com>
2026-02-21 16:31:19 -0800

    fix react issue, tailwind deps issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -54,5 +54,13 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+    // remove with astro v6 https://github.com/withastro/astro/issues/12824#issuecomment-2563095382
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD ? {
+        "react-dom/server": "react-dom/server.edge",
+      } : undefined,
+    },
   },
 })
