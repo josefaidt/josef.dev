57deda978543aab8a1e7ff58fecddea311e1befb
josefaidt <josef.aidt@gmail.com>
2026-02-21 16:50:23 -0800

    tryfix redirect

--- src/pages/resume.pdf.ts
+++ src/pages/resume.pdf.ts
@@ -2,6 +2,7 @@ import type { APIRoute } from "astro"
 
 export const prerender = false
 
-export const GET: APIRoute = () => {
-  return Response.redirect("/josefaidt_resume.pdf", 301)
+export const GET: APIRoute = ({ url }) => {
+  const redirectUrl = new URL("/josefaidt_resume.pdf", url.origin)
+  return Response.redirect(redirectUrl, 301)
 }
